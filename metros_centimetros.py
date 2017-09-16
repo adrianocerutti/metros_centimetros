556a2c907ae3975ed9c6218974bf48976080ee48
Adriano Bernardo Cerutti <adrianocerutti@gmail.com>
2017-09-16 10:08:21 -0300

    Create metros_centimetros.py

---
+++ metros_centimetros.py
@@ -0,0 +1,8 @@
+#Imprime uma mensagem na tela
+print("Conversor de Metros para Centímetros")
+#Recupera o valor digitado pelo usuário e armazena na variável m
+m = float(input("Digite um número: "))
+#Realiza o cálculo para a conversão de metros em centímetros e armazena o resultado na variável c
+c = m * 100
+#Imprime a mensagem de resposta na tela
+print(m,"metro(s) convertido(s) em centímetro(s) é igual a", c,"centímetro(s)")
